ec3b416747bfd1270b28f8dce5ac2febc679e691
Schr0Sm1ley <radiantremel444@gmail.com>
2025-01-22 18:54:33 +0600

    Implemented the API for artifact details page, artifact update and delete , also for updating the likeCount

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const port = process.env.PORT || 3000;
 app.use(cors());
 app.use(express.json());
 
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@schr0smi1ey.iioky.mongodb.net/?retryWrites=true&w=majority&appName=Schr0Smi1ey`;
 
 const client = new MongoClient(uri, {
@@ -20,6 +20,7 @@ const client = new MongoClient(uri, {
 });
 const database = client.db("ArtifactHub");
 const userCollection = database.collection("users");
+const artifactCollection = database.collection("artifact");
 
 async function run() {
   try {
@@ -44,6 +45,47 @@ async function run() {
     app.get("/Users", async (req, res) => {
       const cursor = userCollection.find();
       const result = await cursor.toArray();
+      res.send(result);
+    });
+
+    // Artifacts
+    app.post("/Artifacts", async (req, res) => {
+      const newArtifact = req.body;
+      const result = await artifactCollection.insertOne(newArtifact);
+      res.send(result);
+    });
+    app.get("/Artifacts", async (req, res) => {
+      const cursor = artifactCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+    app.get("/Artifacts/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const visa = await artifactCollection.findOne(query);
+      res.send(visa);
+    });
+    app.put("/Artifacts/:id", async (req, res) => {
+      const id = req.params.id;
+      const artifact = req.body;
+      console.log(artifact);
+      const result = await artifactCollection.updateOne({ _id: new ObjectId(id) }, { $set: artifact });
+      res.send(result);
+    });
+    app.delete("/Artifacts/:id", async (req, res) => {
+      const id = req.params.id;
+      const result = await artifactCollection.deleteOne({
+        _id: new ObjectId(id),
+      });
+      res.send(result);
+    });
+    app.patch("/Artifacts/:id", async (req, res) => {
+      const { id } = req.params;
+      const { likeCount } = req.body;
+      const result = await artifactCollection.updateOne(
+        { _id: new ObjectId(id) },
+        { $set: { likeCount } }
+      );
       res.send(result);
     });
   } finally {
