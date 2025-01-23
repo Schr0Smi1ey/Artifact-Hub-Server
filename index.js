1174a0e2c3cc60b52a5b62c8def544dbbce94f5f
Schr0Sm1ley <radiantremel444@gmail.com>
2025-01-23 18:00:25 +0600

    Implemented the JSON Web Token Functionality

--- index.js
+++ index.js
@@ -1,12 +1,20 @@
 const express = require("express");
 const cors = require("cors");
+const jwt = require("jsonwebtoken");
+const cookieParser = require("cookie-parser");
 const app = express();
 require("dotenv").config();
 const port = process.env.PORT || 3000;
 
 // Middleware
-app.use(cors());
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
 app.use(express.json());
+app.use(cookieParser());
 
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@schr0smi1ey.iioky.mongodb.net/?retryWrites=true&w=majority&appName=Schr0Smi1ey`;
@@ -18,6 +26,20 @@ const client = new MongoClient(uri, {
     deprecationErrors: true,
   },
 });
+
+const verifyToken = (req, res, next) => {
+  const accessToken = req.cookies.accessToken;
+  console.log("Access Token", accessToken);
+  if (!accessToken) return res.status(401).send("Unauthorized Access");
+
+  jwt.verify(accessToken, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+    console.log(err);
+    if (err) return res.status(403).send("Unauthorized Access");
+    req.user = decoded;
+    next();
+  });
+};
+
 const database = client.db("ArtifactHub");
 const userCollection = database.collection("users");
 const artifactCollection = database.collection("artifact");
@@ -36,6 +58,31 @@ async function run() {
       "Pinged your deployment. You successfully connected to MongoDB!"
     );
 
+    // Auth Related API
+    app.post("/jwt", (req, res) => {
+      const user = req.body;
+      const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+        expiresIn: "5h",
+      });
+
+      res
+        .cookie("accessToken", accessToken, {
+          httpOnly: true,
+          secure: process.env.NODE_ENV === "production",
+          sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
+        })
+        .send({ sucess: true });
+    });
+
+    app.post("/logout", (req, res) => {
+      res
+        .clearCookie("accessToken", {
+          httpOnly: true,
+          secure: process.env.NODE_ENV === "production",
+          sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
+        })
+        .send({ success: true });
+    });
     // Users
     app.post("/Users", async (req, res) => {
       const newUser = req.body;
@@ -54,17 +101,30 @@ async function run() {
       const result = await artifactCollection.insertOne(newArtifact);
       res.send(result);
     });
+
     app.get("/Artifacts", async (req, res) => {
       const cursor = artifactCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+    app.get("/my-artifacts", verifyToken, async (req, res) => {
+      const { addedBy } = req.query;
+      console.log(addedBy);
+      if (req.user.email !== addedBy) {
+        console.log(req.user.email);
+        return res.status(403).send("Forbidden Access");
+      }
+      const cursor = artifactCollection.find({ addedBy });
       const result = await cursor.toArray();
       res.send(result);
     });
     app.get("/Artifacts/:id", async (req, res) => {
       const id = req.params.id;
       const query = { _id: new ObjectId(id) };
-      const visa = await artifactCollection.findOne(query);
-      res.send(visa);
+      const artifact = await artifactCollection.findOne(query);
+      res.send(artifact);
     });
+
     app.put("/Artifacts/:id", async (req, res) => {
       const id = req.params.id;
       const artifact = req.body;
@@ -75,6 +135,7 @@ async function run() {
       );
       res.send(result);
     });
+
     app.delete("/Artifacts/:id", async (req, res) => {
       const id = req.params.id;
       const result = await artifactCollection.deleteOne({
@@ -82,6 +143,7 @@ async function run() {
       });
       res.send(result);
     });
+
     app.patch("/Artifacts/:id", async (req, res) => {
       const { id } = req.params;
       const { likeCount } = req.body;
@@ -116,13 +178,16 @@ async function run() {
         res.status(200).json({ message: "Artifact liked successfully." });
       }
     });
-    app.get("/liked-artifacts", async (req, res) => {
+    app.get("/liked-artifacts", verifyToken, async (req, res) => {
       const { user_email } = req.query;
+      if (req.user.email !== user_email) {
+        return res.status(403).send("Forbidden Access");
+      }
       const cursor = likedArtifactCollection.find({ user_email });
       const LikedArtifacts = await cursor.toArray();
 
       const artifactIds = LikedArtifacts.map((item) => item.artifact_id);
-      const result = artifactCollection
+      const result = await artifactCollection
         .find({ _id: { $in: artifactIds.map((id) => new ObjectId(id)) } })
         .toArray();
       res.status(200).send(result);
